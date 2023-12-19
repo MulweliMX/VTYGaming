415627937dde1b109cd548752263c3850916433e
MulweliMX <mavundamulweli18@gmail.com>
2023-12-19 19:20:54 +0200

    cmsUpdate

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,6 +127,20 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+/**
+ * Netlify
+ */
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", (user) => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
+
+
 
 
 
